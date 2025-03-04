9c8cd43adafbd525cc3fcffdd823a7377ef5bc89
Rehankhan-90 <mdrehankhan7071903226@gmail.com>
2025-03-04 07:04:10 +0000

    dataType

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 14455;
+let accountEmail = "rehan@gmail.com";
+var accountPassword = "123455556"
+accountCity = "lucknow";
+let accountState;
+
+// accountId = 2
+
+
+accountEmail = "rk@gmail.com"
+accountPassword = "123456"
+accountCity = "kanpur"
+
+/*
+prefer not to use var
+becouse of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,27 @@
+"use string"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using node.js ,not browser
+
+console.log(3 +3); // code readability shoud be high
+
+console.log("Rehan");
+
+let name = "rehan"
+let age = 24
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
