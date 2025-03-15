6a2847331e36034657ea5345c304d00fd2a07482
Rehankhan-90 <mdrehankhan7071903226@gmail.com>
2025-03-15 11:12:37 +0000

    Nums and Math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,54 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number (100)
+// console.log(balance);
+// 
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 1123.8966
+
+// console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(5));
+// console.log(otherNumber.toPrecision(6));
+
+const one = 1000000
+
+// console.log(one.toLocaleString());
+// console.log(one.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++++++  Maths  ++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 5, 8));
+// console.log(Math.max(4, 3, 5, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
+
+
+
+
+
