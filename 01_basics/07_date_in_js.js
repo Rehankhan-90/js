af4ae7cbc755a87a62aceeb06555aab0ce349d09
Rehankhan-90 <mdrehankhan7071903226@gmail.com>
2025-03-17 10:01:26 +0000

    date in js

---
+++ 01_basics/07_date_in_js.js
@@ -0,0 +1,48 @@
+// ---------------   Date   -------------------
+
+let myDate = new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.getTimezoneOffset());
+// console.log(myDate.toDateString());
+
+
+
+// let myCreatedDate = new Date (2025, 2, 16)
+// let myCreatedDate = new Date (2025, 2, 16, 4, 30)
+// let myCreatedDate = new Date ("2025-03-16")
+let myCreatedDate = new Date ("03-16-2025")
+
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date ()
+
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDate());
+
+// 
+
+console.log(newDate.toLocaleString('default', {
+    weekday: "long",
+}))
+
+
+
+
+
